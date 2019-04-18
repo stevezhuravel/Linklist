7173c1cd7dbbaad727269a00d4143dc1e499fae2
stevezhuravel <33463234+stevezhuravel@users.noreply.github.com>
2019-04-18 11:41:19 -0400

    Add files via upload
    fix methods

--- SentinelLink.java
+++ SentinelLink.java
@@ -1,38 +1,65 @@
 import java.util.*;
 public class SentinelLink<E> {
+	protected Node s;
 	
+	// creating an Iterator class
 	private class MyIterator implements Iterator<E>{		
-		private Node pointer; 
-		
-		public MyIterator() {
+		protected Node nextNode,returned;
+		protected int index;
+		// constructor for iterator
+		public MyIterator(Node pointer,int index) {
 			
-			pointer = s.getNext();
-			
+			// setting next node to pointer
+			 this.nextNode=pointer;
+			 // setting insex to it self keep track
+			 this.index=index;
+	
 		}
 		public boolean hasNext() {
-			return false;
+			return nextNode!=s;
+		}
+		
+		
+		
+		public E prev() {
+			if(!this.hasPrev())throw new NoSuchElementException();
+			E num = nextNode.prev.data;
+			returned=nextNode.prev;
+			nextNode=nextNode.prev;
+			index--;
+			return num;
+		}
+		
+		
+		public boolean hasPrev() {
+			return nextNode.prev !=s;
 		}
 		public E next() {
-			return null;
+			if(!this.hasNext()) throw new NoSuchElementException();
+			E num =nextNode.data;
+			returned=nextNode.prev;
+			nextNode=nextNode.next;
+			index++;
+			return num;
 		}
 		public void remove() {
 			throw new UnsupportedOperationException();
 		}
 	}
+	
 	private class Node{
 		
 		private E data;
-		private Node next, prev;
+		protected Node next, prev;
 		
 		public Node() {}
 		public Node(E data) {
 			
 			this.data = data;
-			this.next = null;
-			this.prev = null;
+
 		}
 		
-		//faisal
+		//
 		public E getData() {
 			return data;
 		}
@@ -63,12 +90,12 @@ public class SentinelLink<E> {
 			// TODO Auto-generated method stub
 			return false;
 		}
-		public Iterator<E> Iterator() {
-			return null;
+		public SentinelLink<E>.MyIterator Iterator() {
+			return new MyIterator(next, len);
 		}
 	}
 	
-	private Node s;
+	
 	public int len;
 	
 	public SentinelLink(){
@@ -76,7 +103,7 @@ public class SentinelLink<E> {
 		s.next = null;
 		s.prev = null;
 	}
-	
+	// prints the linklist out
 	public void print() {
 		Node temp = s;
 		while(temp.next != s) {
@@ -84,6 +111,7 @@ public class SentinelLink<E> {
 			System.out.println(temp.data);
 		}
 	}
+	// tells us if the elements is or is not found
 	public boolean contains(Object o) {
 		Node temp = s.next;
 		
@@ -95,7 +123,8 @@ public class SentinelLink<E> {
 		
 		return false;
 	}
-
+	// removes the first element of the list 
+	//unlocking each node
 	public E removeFirst() {
 		
 		Node temp = s.prev;
@@ -104,7 +133,8 @@ public class SentinelLink<E> {
 		len --;
 		return temp.data;
 	}
-	
+	// add the elements to the frount of the list
+	// increase the length after each add
 	public void addFirst(E data) {
 		Node nmd = new Node(data);
 		
@@ -123,15 +153,17 @@ public class SentinelLink<E> {
 
 		len++;
 	}
-	
+	// construct an empty list
 	public boolean isEmpty() {
 		return len == 0;
 }
+	// return the size of the list 
 	public int size() {
 			return len;
 		
 	}
-	
+	// add the element to a specfic element of the list 
+	// throws indexoutofboundsexception
 	public void add(int ind, E data ) {
 		if(ind > len)
 			throw new IndexOutOfBoundsException();
@@ -144,7 +176,7 @@ public class SentinelLink<E> {
 		else if(ind == len - 1)
 			addLast(data);
 		else {
-			for(int i = 0; i <= ind; i++) 
+			for(int i = 0; i <= ind-1; i++) 
 				temp = temp.next;
 			
 			nmd.next = temp;
@@ -160,18 +192,19 @@ public class SentinelLink<E> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	// add the element to the end of the list
 	public boolean add(E data) {
 		addLast(data);
 		return true;
 	}
-	
+	// clear the list and it sets the size to be zero
 	public void clear() {
 		s.prev = s;
 		s.next = s;
 		len = 0;
 	}
 	
+	// remove the first occurrence of a number in the list
 	public boolean removeFirstOccurrence(Object o) {
 		Node temp = s.next;
 		while(temp.hasNext()) {
@@ -187,6 +220,7 @@ public class SentinelLink<E> {
 		return false;
 	}	
 	
+	// remove the last element the occurrecus in the list
 	public boolean removeLastOccurrence(Object o) {
 		Node temp = s.next;
 		while(temp.hasPrev()){
@@ -202,6 +236,7 @@ public class SentinelLink<E> {
 		return false;
 	}	
 	
+	// remove the last element of the list
 	public E removeLast() {
 		Node temp = s.next;
 		s.prev = s.prev.prev;
@@ -209,6 +244,9 @@ public class SentinelLink<E> {
 		len --;
 		return temp.data;
 	}
+	// Removes the first occurrence of the specified element from this list, if it is present
+	// throw nosuchelementexception
+	// return the first element in the list
 	public E remove() {
 		if(this.isEmpty())
 			throw new NoSuchElementException();
@@ -216,7 +254,8 @@ public class SentinelLink<E> {
 
 		
 	}
-	
+	// remove a specfic element of the list by index
+	// unlocking  nodes
 	public E remove(int index) {
 		Node temp = s;
 		if(index < 0 || index >= size()) throw new IndexOutOfBoundsException();
@@ -230,41 +269,40 @@ public class SentinelLink<E> {
 			len --;
 			return temp.data;
 	}
+	// get the last element but does not remove it 
 	public E getLast() {
 		return s.prev.data;
 	}
-	
+	// get the first element of the list but does not remove
 	public E getFirst(){
 		return s.next.data;
 	}
 	
+	// gets the elements but does not return it 
 	public E element() {
 		return s.next.data;
 	}
-	
+	// retervies the element
 	public E peek() {
 		return element();
 	}
+	// retervies the first element of the list but it does not remove it 
+	// returns the first element of the list
 	public E peekFirst() {
 		
 		if(s.next == null)
 			return null;
 		return element();
 	}
-	
+	// retervies the last element of the list but it does not remove it
+	// return the last element of the list
 	public E peekLast() {
 		if(s.prev == null)
 			return null;
 		return s.prev.data;
 	}
 	
-	public E c() {
-		Node temp = s.next;
-		s.next = s.next.next;
-		s.next.prev = s;
-		len --;
-		return temp.data;
-	}
+	//  replaces the element by index
 	public E set(int index, E element) {
 		Node temp = s.next;
 		
@@ -276,7 +314,8 @@ public class SentinelLink<E> {
 			temp.data = element;
 			return num;
 	}
-	
+	// add the element to the end of the list
+	// unlocking nodes
 	public void addLast(E e) {
 	Node nmd = new Node(e);
 		if(s.next == null) {
@@ -290,9 +329,10 @@ public class SentinelLink<E> {
 			nmd.prev = s.prev;
 			s.prev = nmd;	
 		}
-
+		// increase the size
 		len++;
 	}
+	// removes the head of the element
 	public boolean remove(Object o) {
 		Node temp = s;
 		while(temp.hasNext()) {
@@ -302,7 +342,7 @@ public class SentinelLink<E> {
 		}
 		 return false;
 	}
-	
+	//
 	public boolean addAll(Collection<? extends E> c) {
 		
 		Iterator<? extends E> iter = c.iterator();
@@ -312,52 +352,74 @@ public class SentinelLink<E> {
 		return false;
 	
 		}
-	public boolean addAll(int index, Collection<? extends E> c) {
+	
+	// return the list in descending/backwards order
+	public Iterator<E>	descendingIterator(){
+		// creating an iterator
+		MyIterator listiter = s.Iterator();
+		 while(listiter.hasNext()){
+			 return listiter;
+	}
+		return  listiter;
 		
 	}
 	
-	
-	public Iterator<E>	descendingIterator(){
-		return new MyIterator();
-	
-	}
+	// return the index of the first occuring of the element that you are looking for
 	public int indexOf(Object o) {
-		int count =0;
+		// starting of count at 0
+		int count = 0;
 		Node cmd = s.next;
 		while(cmd.hasNext()) {
-			cmd=cmd.next;
-			count++;
-			
-		   if(cmd.next.data.equals(o)) {
-				return count;
+			    
+		   if(cmd.data.equals(o)) {
+
+				 return count;
 			}
+		   // set node to the next node
+		   cmd = cmd.next;
+	      // increases count
+          count ++;
 		}
+		// returns -1 if it does not find it
 		return -1;
 	}
+	
+	// return the last index of the element that you are looking you
 	public int lastIndexOf(Object o) {
-		int count = 0;
+		// starting count at len - 1
+		int count = len -1;
+		// last node
 		Node cmd = s.prev;
 		while(cmd.hasPrev()) {
-		cmd = cmd.prev;
-		count ++;
-		}
-		if(cmd.prev.data.equals(o)) {
+		if(cmd.data.equals(o)) {
 			return count;
+			}
+			//System.out.println(o + " is not equal to  " + cmd.data);
+			cmd = cmd.prev;
+			count --;
 		}
+		 //returns -1 if the element is not found
 		return -1;
 	}
 	
-	public ListIterator<E>	listIterator(int index){ 
-		/*if(index < 0 || index < size())throw new IndexOutOfBoundsException();
-		Iterator<E> list = s.Iterator();
-		while(list.hasNext()) {
-			list.next();
-			return list.data();
-		}*/
-			
-	}
+	public ListIterator<E>listIterator(int index){
 		
+		if(index < 0 || index > len) throw new IndexOutOfBoundsException();
+		MyIterator listiter = s.Iterator();
+			while(listiter.hasNext()) {
+				listiter.next();
+			}
+			return (ListIterator<E>) listiter;
+				}
+		
+			
+		
+		
+			
 	
+		
+	// add the element to the end as it tail
+	// creating an node reference
 	public boolean offer(E e) {
 		Node temp = s;
 		Node cmd = new Node(e);
@@ -368,16 +430,21 @@ public class SentinelLink<E> {
 			s.prev = cmd;
 			
 		}
+		// returns true if it was able to add the element
 		return true;
 	}
+	// insert the element to the front of the list
 	public boolean	offerFirst(E e) {
 		addFirst(e);
 		return true;
 	}
+	// insert the element to the end of the list
 	public boolean	offerLast(E e) {
 		addLast(e);
+		// retuns true
 		return true;
 	}
+	// retervies and remove the head
 	public E poll() {
 		Node cmd = s.next;
 		if(s.isEmpty())
@@ -388,28 +455,30 @@ public class SentinelLink<E> {
 		return remove();
 		
 	}
+	//retervies and remove the first element of the list
 	public E pollFirst() {
 		if(s.next == null)
 			return null;
 		return remove();
 	
 	}
+	//retervies and remove the last element of the list
 	public E pollLast() {
 		if(s.prev == null)
 			return null;
 		return removeLast();
 	}
-	
+	// removes the last element of the list
 	public E pop() {
 		return removeLast();
 		
 	}
-	
+	// add amd element to the list
 	public void push(E e) {
 		add(e);
 		
 	}
-	
+	// returns and array and it contains all the elements that are in the list 
 	public Object[]	toArray() {
 		Object[] arr  = new Object[len];
 		Node temp = s.next;
@@ -421,12 +490,13 @@ public class SentinelLink<E> {
 		
 		return arr;
 	}
-	
+	// returns and array is order from head to tail
 	public <T> T[]	toArray(T[] a) {
 		if(a.length < len)
 			throw new ArrayStoreException();
 		Node temp = s.next;
 		
+		// loop to go through the array to length
 		for(int i = 0; i < len; i++ ) {
 			a[i] = (T) temp.data;
 			temp = temp.next;
--- TestSentinel.java
+++ TestSentinel.java
@@ -1,146 +1,195 @@
-
+import java.util.*;
 public class TestSentinel {
 
 	public static void main(String[] args) {
-		SentinelLink<Integer> list = new  SentinelLink<Integer>();
+		//SentinelLink<Integer> list = new  SentinelLink<Integer>();
+		// print iterator in decsending order
+		LinkedList list = new LinkedList();
+			
+			
+			//list.addFirst(5);
+			//list.addFirst(3);
+			//list.addFirst(6);
+			//list.addFirst(9);
+			
+			//list.add(2, 10);
+			//list.add(3 , 7);
+			//list.add(1 , 3);
+			//list.add(0, 8);
+			//list.add(list.size(), 5);
+			
 		
+			list.add(7);
+			list.add(12);
+			list.add(25);
+			list.add(18);
+			list.add(4);
+			list.add(17);
+			list.add(17);
+			list.add(17);
+			// added to test lastindexof method
+			list.add(7);
+			list.add(25);
+			list.add(4);
+		/*list.addLast(7);
+			//list.addLast(12);
 			
-			
-			list.addFirst(5);
-			list.addFirst(3);
-			list.addFirst(6);
-			list.addFirst(9);
-			
-			list.add(2, 10);
-			list.add(3 , 7);
-			list.add(1 , 3);
-			list.add(0, 8);
-			list.add(list.size(), 5);
-			
-			list.addLast(7);
-			list.addLast(12);
 			
 			
 			
 			//remove the last elements
-			//list.removeLast();
+			list.removeLast();
 			
 			
 			// remove the last element
-			//list.removeLast();
+			list.removeLast();
 			
 			//remove the first element
-			//list.removeFirst();
+			list.removeFirst();
 			
 			// pushes the elements onto the stack
-			//list.push(12);
-			//list.push(3);
+			list.push(12);
+			list.push(3);
 			
-			// i have //
-			//list.poll();
-			//list.clear();
 			
-			//list.remove();
+			list.poll();
+			list.clear();
+			
+			list.remove();
 			 
 			
-		
-			//list.pop();
-			//list.pop();
+			// pop the element out of the list
+			list.pop();
+			list.pop();
 			
-			//System.out.println("clear " + list.size());
-			//System.out.println("poll " + list.size());
-			//System.out.println(" size: " + list.size());
-			//System.out.println(" push " + list.size());
+			System.out.println("clear " + list.size());
+			System.out.println("poll " + list.size());
+			System.out.println(" size: " + list.size());
+			System.out.println(" push " + list.size());
 			
-			//System.out.println(list.remove(3));
-			//System.out.println("number removed" + list.remove());
-			//System.out.println(" size: " + list.size());
-			//System.out.println("remove number " + list.removeFirst());
-			//System.out.println("remove number " + list.removeLast());
-			//System.out.println("removed firstOccurrence " + list.removeFirstOccurrence(new Integer (9)));
-			//System.out.println(" size: " + list.size());
+			System.out.println(list.remove(3));
+			System.out.println("number removed" + list.remove());
+			System.out.println(" size: " + list.size());
+			System.out.println("remove number " + list.removeFirst());
+			System.out.println("remove number " + list.removeLast());
+			System.out.println("removed firstOccurrence " + list.removeFirstOccurrence(new Integer (9)));
+			System.out.println(" size: " + list.size());
 			
 			
-			//System.out.println("remove lastoccurrence " + list.removeLastOccurrence(new Integer (3)));
+			System.out.println("remove lastoccurrence " + list.removeLastOccurrence(new Integer (3)));
 			
 			//returns the last elemnet of the list
-			//System.out.println("remove last " + list.removeLast());
+			System.out.println("remove last " + list.removeLast());
 			
 			//removes and return the first elements
-			//System.out.println("remove first " + list.removeFirst());
+			System.out.println("remove first " + list.removeFirst());
 			
 			// retrives and  but does not removes the head
-			//System.out.println("peek " + list.peek());
+			System.out.println("peek " + list.peek());
 			
 			// get the last elements
-			//System.out.println("get last " + list.getLast());
+			System.out.println("get last " + list.getLast());
 			
 			// get the first elements
-			//System.out.println("get first " + list.getFirst());
+			System.out.println("get first " + list.getFirst());
 			
-			//System.out.println("index element," + list.set(0, 5));
-			/*System.out.println("Object array");
+			System.out.println("index element," + list.set(0, 5));
+			System.out.println("Object array");
 			for(Object o : list.toArray())
 				System.out.println(o);
 			
 			System.out.println("Regular list");
-			*/
-			/*Integer[] a = new Integer[list.size()];
+			
+			Integer[] a = new Integer[list.size()];
 			for (Integer i : list.toArray(a))
-			System.out.println(i);*/
+			System.out.println(i);
 			//System.out.println(list.addAll(4));
 			//tells us if an elements exisit in a list
-			//System.out.println("contains " + list.contains(5));
-			//System.out.println("offer first " + list.offerFirst(8));
-			//System.out.println("offer first " + list.offerFirst(29));
-			//System.out.println("offer last " + list.offerLast(12));
-			//System.out.println("offer last " + list.offerLast(32));
+			System.out.println("contains " + list.contains(5));
+			System.out.println("offer first " + list.offerFirst(8));
+			System.out.println("offer first " + list.offerFirst(29));
+			System.out.println("offer last " + list.offerLast(12));
+			System.out.println("offer last " + list.offerLast(32));
 			
 			// remove method by index
-			//System.out.println("remove by index " + list.remove(4));
+			System.out.println("remove by index " + list.remove(4));
 			
 			// peek first return but does not remove the first elements and returns null if it empty
-			//System.out.println("peek first " + list.peekFirst());
+			System.out.println("peek first " + list.peekFirst());
 			
 			// peek last return but does not remove the last elements or returns null if it empty
 		//System.out.println("peek last " + list.peekLast());
 			
 			//indexOf return the first occurance or return -1
-			//System.out.println("indexOf " + list.indexOf(5));
+			System.out.println("indexOf " + list.indexOf(5));
+			
 			
 			//poll first retrivrs  and removes the first element or return null if it empty
-			//System.out.println("poll first " + list.pollFirst());
-			//System.out.println("poll first " + list.pollFirst());
+			System.out.println("poll first " + list.pollFirst());
+			System.out.println("poll first " + list.pollFirst());
 			
 			//poll last retrieves and removes the first element or return null if it empty
-			//System.out.println("poll last " + list.pollLast());
-			//System.out.println("poll last " + list.pollLast());
+			System.out.println("poll last " + list.pollLast());
+			System.out.println("poll last " + list.pollLast());
 			
 			// offer add the element to the tail
-			//System.out.println("offer " + list.offer(9));
+			System.out.println("offer " + list.offer(9));
 			
 			//set replace the element at the specfic position
 			
 			//System.out.println(" size: " + list.size(9));
 			
 			//checks if the element is found or not found
-			//System.out.println("contains " + list.contains(new Integer(12)));
+			System.out.println("contains " + list.contains(new Integer(12)));
 			
 			//checks if the element is found or not found
-			//System.out.println("contains " + list.contains(new Integer(8)));
+			System.out.println("contains " + list.contains(new Integer(8)));
 			
 			//checks if the element is found or not found
-			//System.out.println("contains " + list.contains(new Integer(18)));
+			System.out.println("contains " + list.contains(new Integer(18)));
 			
 			//checks if the element is found or not found
 			System.out.println("contains " + list.contains(new Integer(58)));
-			list.print();
 			
+
+			
+			Iterator p = list.descendingIterator();
+				while(p.hasNext())
+			System.out.println("descendingIterator :" + p.next());
+				
+			list.descendingIterator();
+			
+			
+			//System.out.println();
+			// index of
+			 System.out.println(" index of 17 is  " + list.indexOf(new Integer(17)));
+			
+			//add it to the end of the list and return true
+			System.out.println("offer 23 " + list.offer(23));
+			//add it to the end of the list and return true
+
+			System.out.println("offer 13 " + list.offer(13));
+
+			// last occuring
+			System.out.println("last occuring index of  is " + list.lastIndexOf(new Integer(7)));
+			System.out.println("last index of 18 is  " + list.indexOf(new Integer(28)));
+			System.out.println("last index of 12 is  " + list.indexOf(new Integer(12)));
+			System.out.println("last index of 4 is  " + list.indexOf(new Integer(4)));
+			System.out.println("last index of 58 is  " + list.indexOf(new Integer(58)));
+			
+			//last index of 
+			System.out.println("last index of 46 is  " + list.lastIndexOf(new Integer(46)));
+			System.out.println("last index of 42 is  " + list.lastIndexOf(new Integer(42)));
+			System.out.println("last index of 17 is  " + list.lastIndexOf(new Integer(17)));
+			System.out.println("last index of 25 is  " + list.lastIndexOf(new Integer(25)));*/
+			Iterator p = list.listIterator();
+			while(p.hasNext())
+		System.out.println("listiterator :" + p.next());
+			//list.print();
 			
 			if(list.isEmpty())
 				System.out.println("the list is empty");
 			else 
 				System.out.println("the list is not empty ");
 	}
-
 }
